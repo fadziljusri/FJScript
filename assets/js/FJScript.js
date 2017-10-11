f66e107e54b1019641bbf00a5b4c52ae3c62784c
fadziljusri <fadziljusri@gmail.com>
2017-10-11 17:16:50 +0800

    add more functions

--- assets/js/FJScript.js
+++ assets/js/FJScript.js
@@ -1,8 +1,8 @@
 ; (function (global, $) {
 
     // 'new' an object 
-    var FJScript = function (firstName, lastName, lang) {
-        return new FJScript.init(firstName, lastName, lang);
+    var FJScript = function (firstName, lastName, email, tel, web, message, lang) {
+        return new FJScript.init(firstName, lastName, email, tel, web, message, lang);
     }
 
     var supportedLangs = ['en', 'ms', 'es', 'ar', 'hi', 'zh', 'fa'];
@@ -27,6 +27,66 @@
         fa: 'وارد شده'
     }
 
+    var formLangs = {
+        en: {
+            fn: 'First Name',
+            ln: 'Last Name',
+            email: 'Email',
+            tel: 'Phone Number',
+            web: 'Web Site',
+            msj: 'Type your message here..'
+        },
+        ms: {
+            fn: 'Nama Pertama',
+            ln: 'Nama Akhir',
+            email: 'Emel',
+            tel: 'No Telefon',
+            web: 'Laman Sesawang',
+            msj: 'Kata kata terakhir (sebelum mati)..'
+        },
+        es: {
+            fn: 'Nombre de pila',
+            ln: 'Apellido',
+            email: 'Email',
+            tel: 'Número de teléfono',
+            web: 'Sitio web',
+            msj: 'Escribe tu mensaje aquí'
+        },
+        ar: {
+            fn: 'الاسم الاول',
+            ln: 'الكنية',
+            email: 'البريد الإلكتروني',
+            tel: 'رقم هاتف',
+            web: 'موقع الكتروني',
+            msj: 'اكتب رسالتك هنا'
+        },
+        hi: {
+            fn: 'पहला नाम',
+            ln: 'अंतिम नाम',
+            email: 'ईमेल',
+            tel: 'टेलीफोन नंबर',
+            web: 'वेबसाइट',
+            msj: 'अपना संदेश यहां टंकित करें'
+        },
+        zh: {
+            fn: '名字',
+            ln: '姓',
+            email: '电子邮件',
+            tel: '电话号码',
+            web: '网站',
+            msj: '在这里输入你的消息'
+        },
+        fa: {
+            fn: 'نام کوچک',
+            ln: 'نام خانوادگی',
+            email: 'پست الکترونیک',
+            tel: 'شماره تلفن',
+            web: 'سایت اینترنتی',
+            msj: 'پیام خود را اینجا بنویسید'
+        },
+
+    }
+
     // prototype holds methods (save memory space) 
     FJScript.prototype = {
         // 'this' refers to the calling object at execution time  
@@ -40,6 +100,7 @@
         },
         // retrieve messages from obj by referring to properties using [] 
         greeting: function () {
+            // let info = '';
             return greetings[this.lang] + ' ' + this.firstName;
         },
         greet: function () {
@@ -49,6 +110,20 @@
             }
             // make chainable 
             return this;
+        },
+        information: function(){
+            var self = this;
+            var info ='';
+
+            info += formLangs[self.lang].email + ' :<br> ' + self.email + ' <br><br>';
+            info += formLangs[self.lang].tel + ' :<br> ' + self.tel + ' <br><br>';
+            info += formLangs[self.lang].web + ' :<br> ' + self.web + ' <br><br>';
+            info += formLangs[self.lang].msj + ' :<br> ' + self.message + ' <br><br>';            
+            
+            return info;
+        },
+        formLang: function () {
+            return formLangs[this.lang];
         },
         log: function () {
             if (console) {
@@ -71,21 +146,46 @@
                 throw "Missing jQuery selector";
             }
             var msg = this.greeting();
-
+            // console.log(msg);
             // inject msg in the chosen place in DOM (Document Object Model) 
             $(selector).html(msg);
 
             return this;
+        },
+        HTMLInfo: function (selector) {
+            if (!selector) {
+                throw "Missing jQuery selector";
+            }
+
+            var info = this.information();
+            console.log(info);
+            $(selector).html(info);
+
+            return this;
+        },
+        setHTMLFormLang: function () {
+            var selFormLang = this.formLang();
+            // console.log(selFormLang);
+            $('input[name="firstName"]').attr('placeholder', selFormLang.fn);
+            $('input[name="lastName"]').attr('placeholder', selFormLang.ln);
+            $('input[name="email"]').attr('placeholder', selFormLang.email);
+            $('input[name="tel"]').attr('placeholder', selFormLang.tel);
+            $('input[name="web"]').attr('placeholder', selFormLang.web);
+            $('textarea[name="message"]').attr('placeholder', selFormLang.msj);
         }
 
     };
 
     // Function constructors - the actual obj is created here, allowing to use 'new' an obj without calling 'new'
-    FJScript.init = function (firstName, lastName, lang) {
+    FJScript.init = function (firstName, lastName, email, tel, web, message, lang) {
 
         var self = this;
-        self.firstName = firstName || '';
-        self.lastName = lastName || '';
+        self.firstName = firstName || '&lt;First Name';
+        self.lastName = lastName || '&lt;Last Name&gt;';
+        self.email = email || '&lt;Email&gt;';
+        self.tel = tel || '&lt;Tel&gt;';
+        self.web = web || '&lt;Web Site&gt;'
+        self.message = message || '&lt;Your Message&gt;';
         self.lang = lang || 'en';
 
         self.validate();
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,20 @@
+var data = [];
 
-$('#lang').change(function(){
-    var me = FJ('Fadzil', 'Jusri');
-    $('#links').show();
-    me.setLang($('#lang').val()).HTMLGreeting('#greeting').log();
+$('#lang').change(function () {
+    
+    var init = FJ();
+    init.setLang($('#lang').val()).setHTMLFormLang();
+    $('#contact').show();
 });
+
+function onSubmit() {
+    $('#contact').hide();
+    data = $('#contact').serializeArray(); 
+    // console.log(data[0]);
+    var me = FJ(data[0].value, data[1].value, data[2].value, data[3].value, data[4].value, data[5].value);
+    me.setLang($('#lang').val()).HTMLGreeting('#greeting').HTMLInfo('#info').log();
+
+    $('#info').show();    
+
+    return false; //don't submit
+}
